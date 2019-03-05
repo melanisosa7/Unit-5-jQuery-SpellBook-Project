4f3e3e52f6afe82269b4b9dd16e37e0260f01c92
melanisosa7 <43611226+melanisosa7@users.noreply.github.com>
2019-03-05 09:31:45 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#invocation").click(function(){
+ $().show();
+});
+
+
+
+//Spell #2
+
+$("      ").click(function(){
+ 
+});
+
+
+
+//Spell #3
+
+$("      ").click(function(){
+ 
+});
+
+
+
